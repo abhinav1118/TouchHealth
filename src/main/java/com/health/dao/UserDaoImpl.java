751173a6d61db8b2957ee152630ddd111a882ab4
ABHINAV GOUD <ABHINAV GOUD@ABHI>
2018-08-02 10:32:45 +0530

    log

--- src/main/java/com/health/dao/UserDaoImpl.java
+++ src/main/java/com/health/dao/UserDaoImpl.java
@@ -1,8 +1,14 @@
 package com.health.dao;
 
+import org.hibernate.Session;
+
+import com.health.utility.HibernateUtility;
+
 public class UserDaoImpl implements UserDao {
 
 	public void RegisterUser() {
+		
+		Session session = HibernateUtility.getSessionFactory().openSession();
 		
 		
 	}
